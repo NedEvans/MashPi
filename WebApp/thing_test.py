e3eb0fc48222e1c79753df78b8acdf6a2bd66913
NedEvans <neil.evans@gmail.com>
2020-03-24 22:55:36 +0800

    added pump visual to ui

--- WebApp/thing_test.py
+++ WebApp/thing_test.py
@@ -1,7 +1,11 @@
 import time
-
 import mashthing
+import RPi.GPIO as GPIO
+import threading
 
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Create the pi thing.
 mash_thing = mashthing.MashThing()
@@ -9,6 +13,18 @@ mash_thing = mashthing.MashThing()
 # Print the current switch state.
 switch = mash_thing.read_switch()
 print('Switch status: {0}'.format(switch))
+
+# Print the current pump state.
+pump = mash_thing.read_pump()
+print('Pump status: {0}'.format(pump))
+
+#def stopFillingHLT(channel):
+ #   print('Stop Filling HLT')
+ #  mash_thing.set_led(True)
+ #   time.sleep(3000)
+ #   GPIO.input(21,False)
+
+#GPIO.add_event_detect(21, GPIO.RISING, callback=stopFillingHLT, bouncetime=1000)
 
 # Now loop forever blinking the LED and reading temp/humidity.
 print('Looping with LED blinking (Ctrl-C to quit)...')
